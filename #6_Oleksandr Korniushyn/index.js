f8c6949ea8c8c19ef0927e3bf5283a3a9820a978
Alex Korniushyn <kornushinsanya@gmail.com>
2023-07-02 13:30:13 +0300

    removed else completely

--- #6_Oleksandr Korniushyn/index.js
+++ #6_Oleksandr Korniushyn/index.js
@@ -2,9 +2,7 @@ function pow(num, degree) {
   if (degree == 1) {
     return num;
   }
-  {
-    return num * pow(num, degree - 1);
-  }
+  return num * pow(num, degree - 1);
 }
 
 console.log(pow(6, 2));
