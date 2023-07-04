fedea407ebad1386c527d5dee3584782fbd3af23
Alex Korniushyn <kornushinsanya@gmail.com>
2023-07-04 18:12:32 +0300

    Quiz

---
+++ #7_Oleksandr Korniushyn/index.js
@@ -0,0 +1,84 @@
+const QUESTIONS = [
+  {
+    question: "Сколько хромосом у здорового человека?",
+    answer: "46",
+    type: "prompt",
+  },
+  {
+    question: "Путин - хуйло?",
+    answer: true,
+    type: "confirm",
+  },
+  {
+    question: "Сколько хромосом у Путина?",
+    answer: 47,
+    type: "prompt",
+  },
+  {
+    question: "Сколько тупых овец в московии (в млн)?",
+    answer: 144,
+    type: "prompt",
+  },
+  {
+    question: "Снесли ли памятник Екатерине-2 в Одессе?",
+    answer: true,
+    type: "confirm",
+  },
+  {
+    question: "Сколько черных пакетов выделяются на одного орка?",
+    answer: 1,
+    type: "prompt",
+  },
+  {
+    question: "На сколько вы оцениваете работу ЗСУ от 1 до 10?",
+    answer: 10,
+    type: "prompt",
+  },
+  {
+    question: "Со скольких позиций готовилось нападение на Беларусь?",
+    answer: 4,
+    type: "prompt",
+  },
+  {
+    question: "Нужно ли сжигать сосийский флаг?",
+    answer: true,
+    type: "confirm",
+  },
+  {
+    question: "Поддерживаете ли вы уход иностранных компаний из московии?",
+    answer: true,
+    type: "confirm",
+  },
+  {
+    question: "Считаете ли вы сосию своим домом?",
+    answer: false,
+    type: "confirm",
+  },
+];
+
+let correctAnswer = 10;
+
+let incorrectAnswer = 0;
+
+for (let i = 0; i < QUESTIONS.length; i++) {
+  answer = QUESTIONS[i].question;
+
+  if (QUESTIONS[i].type == "prompt") {
+    prompt(answer);
+  } else if (QUESTIONS[i].type == "confirm") {
+    confirm(answer);
+  } else {
+  }
+}
+
+let sumOfAnswerScore = QUESTIONS.reduce((acc, answer) => {
+  // if (answer === QUESTIONS.answer) {
+  //   correctAnswer;
+  // } else {
+  //   incorrectAnswer;
+  // }
+
+  return acc + correctAnswer + incorrectAnswer;
+}, 0);
+
+alert(` your score ${sumOfAnswerScore}`);
